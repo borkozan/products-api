0a864bc6b7a3fbc8f815801f624129f883392c34
Žan-Borna Borko <zan.borna@gmail.com>
2026-01-19 18:31:09 +0100

    Added Product model -Defined core product properties used by the frontend

---
+++ Products.Api/Models/Product.cs
@@ -0,0 +1,14 @@
+namespace Products.Api.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public string Thumbnail { get; set; } = string.Empty;
+}
